a44316e82051712f74ed2a759ada38cc1f031d4e
IshaiAsor <ishaiasor@gmail.com>
2017-11-06 13:22:33 +0200

    mocha tests

---
+++ data.js
@@ -0,0 +1,9 @@
+var heroes = [
+	{id:1,name:'moses'},
+	{id:2,name:'jody'},
+	{id:3,name:'jany'},
+	{id:4,name:'gon'},
+	{id:5,name:'ishai'}
+];
+
+module.exports = heroes;
--- server.js
+++ server.js
@@ -1,79 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-var heroes = require('./data.js');
+
+require('./heroes.js')(app);
+
 app.use(express.static('public'));
 
-http.listen(3000, function(){
+module.exports = http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
-
-app.get('/heroes', function(req, res){
-	res.send(heroes);
-});
-
-app.get('/heroes/:id',function (req,res) {
-  return FindHero(req,res);
-});
-
-app.put('/heroes/:id/:name',function (req,res) {
-	const hero = heroes.find(function (hero) {
-		return hero.id == req.params.id;
-	});
-	if(!hero)
-		res.sendStatus(204);
-	hero.name = req.params.name;
-	return res.send(heroes);
-});
-
-app.post('/heroes/:id/:name',function (req,res) {
-	if(req.params.id.length == 0 || req.params.name.length == 0)
-		res.sendStatus(400);
-	if(heroes.find(function (t) { return t.id == req.params.id ; }))
-		res.sendStatus(207);
-	heroes.push({id:req.params.id,name:req.params.name});
-	res.sendStatus(200);
-});
-
-app.delete('/heroes/:id',function (req,res) {
-	if(!heroes.find(function (t) { return t.id == req.params.id ; }))
-	{
-		res.sendStatus(404);
-	}
-	else
-	{
-	findAndRemove(heroes,'id',req.params.id)
-		res.send(heroes);
-	}
-
-});
-
-app.delete('/heroes',function (req,res) {
-	if(!heroes.find(function (t) { return t.name == req.query.name ; }))
-	{
-		res.sendStatus(404);
-	}
-	else
-	{
-		findAndRemove(heroes,'name',req.query.name)
-		res.send(heroes);
-	}
-});
-
-function FindHero(req,res){
-	const id = req.params.id;
-	const hero = heroes.find(function (t) { return t.id == id });
-	if(!hero)
-		res.status(404).send('no hero found for this id');
-	else
-		res.send(hero);
-}
-
-function findAndRemove(array, property, value) {
-	array.forEach(function(result, index) {
-		
-		if(result[property] == value||result[property] === value) {
-			array.splice(index, 1);
-		}
-	});
-}
